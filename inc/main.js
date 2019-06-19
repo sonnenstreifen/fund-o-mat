4e48a1e9f6c3c1257417448d82f517f8175457cf
Sonnenstreifen <moritz@sonnenstreifen.de>
2019-06-19 15:45:51 +0200

    version 1.0

--- inc/main.js
+++ inc/main.js
@@ -1,10 +1,11 @@
 var app = new Vue({
-  el: '.lightning',
+  el: '#lightning',
   data: {
     currentFunding: 0,
     pastFundingTargets: [],
-    currentFundingTargets: [],
-    futureFundingTargets: [],
+    fundingTargets: [],
+    currentFundingTarget: [],
+    lastFundingIndex: 0,
   },
   methods: {
     fundingPercentage: function (fundingTarget) {
@@ -14,18 +15,70 @@ var app = new Vue({
       }
       return this.percentage;
     },
-    addNewFundingTarget: function () {
-      app.pastFundingTargets.push(app.currentFundingTargets.shift());
-      app.currentFundingTargets.push(app.futureFundingTargets.shift());
+    addNewFundingTarget: function (fundingTarget) {
+      if (fundingTarget.fundingTarget > app.currentFundingTarget.fundingTarget) {
+        app.currentFundingTarget.percentage = 0;
+        let percentage = app.fundingPercentage(fundingTarget.fundingTarget);
+        app.pastFundingTargets.push(app.currentFundingTarget);
+        app.currentFundingTarget = fundingTarget;
+        setTimeout(function() {
+          app.setActive();
+          app.currentFundingTarget.percentage = percentage;
+        }, 100);
+      }
+    },
+    setNewFunding: function () {
+      let wait = 0;
+      let i = app.lastFundingIndex;
+      let once = true;
+      let last = i;
+      if (i > 0) {
+        once = false;
+      }
+      for (i = app.lastFundingIndex; i < app.fundingTargets.length; i++) {
+        let fundingTarget = app.fundingTargets[i];
+        if (app.currentFunding >= fundingTarget.fundingTarget) {
+
+          let next = i + 1;
+          if (next in app.fundingTargets) {
+            wait = wait + 2100;
+            app.currentFundingTarget.percentage = 100;
+            setTimeout(function() {
+              app.addNewFundingTarget(app.fundingTargets[next]);
+            }, wait);
+            last = next;
+          }
+        } else {
+          if (once) {
+            setTimeout(function() {
+              app.addNewFundingTarget(app.fundingTargets[last]);
+            }, wait);
+            once = false;
+          }
+        }
+        app.lastFundingIndex = last;
+      }
+      wait = wait + 2100;
+      setTimeout(function() {
+        app.resetActive();
+      }, wait);
+    },
+    getFundingTitle: function () {
+      return this.currentFundingTarget.fundingTitle;
     },
     getPercentage: function () {
-      return this.currentFundingTargets[0].percentage + '%';
+      return this.currentFundingTarget.percentage + '%';
     },
     setActive: function () {
       document.querySelector('.progress').classList.add('active');
     },
     resetActive: function () {
       document.querySelector('.progress').classList.remove('active');
+    },
+    getOverallPercentage: function (fundingTarget) {
+      let percentage = (fundingTarget / app.currentFunding) *
+                       (app.currentFunding / app.currentFundingTarget.fundingTarget) * 100;
+      return percentage + "%";
     },
     listenForFunding: function () {
       var request = new XMLHttpRequest();
@@ -34,14 +87,12 @@ var app = new Vue({
         if (request.readyState === 4 && request.status === 200) {
           var json = JSON.parse(request.responseText);
 
-          if (json.donations && json.donations != app.currentFunding) {
-            console.log("new donations amount: " + json.donations);
+          if (json.donations && json.donations > app.currentFunding) {
             app.currentFunding = json.donations;
           }
         }
 
       };
-//      var requestUrl = window.location.href + "inc/lightningTip.php";
       request.open("POST", requestUrl, true);
       request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
       var params = "Action=getdonations";
@@ -50,27 +101,9 @@ var app = new Vue({
   },
   watch: {
     currentFunding: function() {
-      app.setActive();
-      app.currentFundingTargets[0].percentage =
-        app.fundingPercentage(app.currentFundingTargets[0].fundingTarget);
-      if (app.currentFunding >= app.currentFundingTargets[0].fundingTarget) {
-        setTimeout(function() {
-          app.addNewFundingTarget();
-          setTimeout(function() {
-            app.currentFundingTargets[0]
-              .percentage = app
-                .fundingPercentage(app
-                  .currentFundingTargets[0].fundingTarget);
-            app.setActive();
-            setTimeout(function() {
-              app.resetActive();
-            }, 2000);
-          }, 100);
-        }, 2000);
-      }
-      setTimeout(function() {
-        app.resetActive();
-      }, 1800);
+      this.setActive();
+      app.currentFundingTarget.percentage = app.fundingPercentage(app.currentFundingTarget.fundingTarget);
+      app.setNewFunding();
     }
   },
   mounted: function() {
@@ -85,15 +118,15 @@ var app = new Vue({
     var lang = document.documentElement.lang;
     loadJSON(function(response) {
       var fundingTargets = JSON.parse(response);
-      app.currentFundingTargets.push(fundingTargets.shift());
-      app.futureFundingTargets = fundingTargets;
+      app.fundingTargets = fundingTargets;
+      app.currentFundingTarget = fundingTargets[0];
     }, lang);
   }
 });
 
 function loadJSON(callback, lang) {
   var xobj = new XMLHttpRequest();
-      xobj.overrideMimeType("application/json");
+  xobj.overrideMimeType("application/json");
   xobj.open('GET', './inc/' + lang + '_fundingTargets.json', true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
